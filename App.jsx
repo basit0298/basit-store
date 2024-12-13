32635c522fb283d8d9dd5920f48597365e6ddef7
basit0298 <Agboolahammed1@gmail.com>
2024-12-13 16:53:49 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import React from 'react';
+import './App.css';
+import ProductPage from "./pages/ProductPage"; 
+
+function App() {
+  return (
+    <div className="App">
+      <ProductPage /> 
+    </div>
+  );
+}
+
+export default App;
+
+
